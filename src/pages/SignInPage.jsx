4f9a0265d60a5e8121608dc8ff6dc21b82adf185
aifactoryai23-ai <ai.factoryai23@gmail.com>
2025-11-07 15:27:36 +0300

    Delete src directory

--- src/pages/SignInPage.jsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/clerk-react";
-
-export default function SignInPage() {
-  return (
-    <div style={{ display: "flex", justifyContent: "center", marginTop: "4rem" }}>
-      <SignIn routing="path" path="/sign-in" />
-    </div>
-  );
-}
--- src/pages/SignUpPage.jsx
+++
@@ -1,9 +0,0 @@
-import { SignUp } from "@clerk/clerk-react";
-
-export default function SignUpPage() {
-  return (
-    <div style={{ display: "flex", justifyContent: "center", marginTop: "4rem" }}>
-      <SignUp routing="path" path="/sign-up" />
-    </div>
-  );
-}
