c038769027b0347b8830c0f903d5549a7a9c0061
aifactoryai23-ai <ai.factoryai23@gmail.com>
2025-11-09 16:21:08 +0300

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  base: "/", // ✅ абсолютный путь для SPA на Vercel
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+    // 🔥 Добавь это — чтобы Vite и Rollup понимали JSX/TSX файлы при сборке
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+  },
+  build: {
+    outDir: "dist", // папка для билда
+    emptyOutDir: true, // очищать перед сборкой
+    rollupOptions: {
+      input: "./index.html",
+    },
+  },
+  server: {
+    fs: { allow: [".."] },
+  },
+});
+
+
+
