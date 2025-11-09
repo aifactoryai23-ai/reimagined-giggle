c41afa2f0f46db8d0a9c8bb0873cfc2ce5d3300c
aifactoryai23-ai <ai.factoryai23@gmail.com>
2025-11-09 22:19:58 +0300

    Delete src directory

--- src/components/ui/skeleton.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { cn } from "@/utils";
-
-export function Skeleton({ className, ...props }) {
-  return <div className={cn("animate-pulse rounded-lg bg-gray-200", className)} {...props} />;
-}
-
-export default Skeleton;
